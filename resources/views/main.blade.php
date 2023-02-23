1a2f7ef95f9c0dff12c03f2383becc0f6c7bf0aa
Mashtalir Sahsa <mashtalir_sasha@ukr.net>
2023-02-23 13:45:03 +0200

    Update main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -32,7 +32,21 @@ if(isset($_GET['utm_campaign'])){$utm_campaign=$_GET['utm_campaign'];setcookie("
 	<link rel="preload" as="font" href="/fonts/Futura-Bold/Futura-Bold.woff"  crossorigin>
 	<link rel="preload" as="font" href="/fonts/Futura-Medium/Futura-Medium.woff"  crossorigin>
 
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-K3SVQK4');</script>
+	<!-- End Google Tag Manager -->
+
 </head>
+
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3SVQK4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <nav class="nav">
 	<div class="container">
